bb1bb7cbdfbbb6236968db68b79ef4ba46077147
Merkie <merkie@post.com>
2023-05-31 19:57:35 -0500

    add CORS

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,6 +9,7 @@ const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/v1", routes);
 
