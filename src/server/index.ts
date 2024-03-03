7f0893f207ef06a328d3b5702410926ea77db849
merkie <merkie@post.com>
2024-03-03 17:10:40 -0600

    remove boilerplate comments

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,15 +5,12 @@ const port = 3000;
 
 const router = express.Router();
 
-// Define a simple route
 router.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-// Use routes
 app.use("/", router);
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`FreeSpeech Server running at http://localhost:${port}`);
 });
