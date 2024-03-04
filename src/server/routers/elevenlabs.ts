c58249386cc9c94bf1dc5e6a1b1b676ac54ebb67
merkie <merkie@post.com>
2024-03-03 18:15:16 -0600

    add speak route

--- src/server/routers/elevenlabs.ts
+++ src/server/routers/elevenlabs.ts
@@ -12,3 +12,13 @@ elevenlabsRouter.get('/voices/list', async function (req, res) {
 		voices
 	});
 });
+
+elevenlabsRouter.post('/speak', async function (req, res) {
+	if (!process.env.ELEVEN_LABS_KEY) return res.json({ error: 'Eleven Labs API key not found.' });
+
+	const { voice_id, text } = req.body;
+	const audio = await elevenlabs.speak(process.env.ELEVEN_LABS_KEY, voice_id, text);
+
+	res.set('Content-Type', 'audio/mpeg');
+	res.send(Buffer.from(audio));
+});
