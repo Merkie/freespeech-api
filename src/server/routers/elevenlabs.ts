ed25953e22de9ad782e75ae4cded951b07ba95f7
merkie <merkie@post.com>
2024-03-03 18:20:04 -0600

    allow for custom api keys

--- src/server/routers/elevenlabs.ts
+++ src/server/routers/elevenlabs.ts
@@ -4,20 +4,23 @@ import elevenlabs from '../../elevenlabs';
 export const elevenlabsRouter = Router();
 
 elevenlabsRouter.get('/voices/list', async function (req, res) {
-	if (!process.env.ELEVEN_LABS_KEY)
-		return res.json({ error: 'Eleven Labs API key not found.', voices: [] });
+	let apiKey = (req.headers['elevenlabs-key'] as string) || process.env.ELEVEN_LABS_KEY;
 
-	const voices = await elevenlabs.voices.list(process.env.ELEVEN_LABS_KEY);
-	return res.json({
+	if (!apiKey) return res.json({ error: 'Eleven Labs API key not found.', voices: [] });
+
+	const voices = await elevenlabs.voices.list(apiKey);
+	res.json({
 		voices
 	});
 });
 
 elevenlabsRouter.post('/speak', async function (req, res) {
-	if (!process.env.ELEVEN_LABS_KEY) return res.json({ error: 'Eleven Labs API key not found.' });
+	let apiKey = (req.headers['elevenlabs-key'] as string) || process.env.ELEVEN_LABS_KEY;
+
+	if (!apiKey) return res.json({ error: 'Eleven Labs API key not found.' });
 
 	const { voice_id, text } = req.body;
-	const audio = await elevenlabs.speak(process.env.ELEVEN_LABS_KEY, voice_id, text);
+	const audio = await elevenlabs.speak(apiKey, voice_id, text);
 
 	res.set('Content-Type', 'audio/mpeg');
 	res.send(Buffer.from(audio));
