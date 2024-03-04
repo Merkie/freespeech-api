1571bf6e36f5da514959e50ac02994a09144790e
merkie <merkie@post.com>
2024-03-03 18:15:10 -0600

    add text to speech function

--- src/elevenlabs/funcs/TextToSpeech.ts
+++ src/elevenlabs/funcs/TextToSpeech.ts
@@ -1,1 +1,22 @@
-export default async function TextToSpeech() {}
+export default async function TextToSpeech(apiKey: string, voiceId: string, text: string) {
+	const response = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Accept: 'audio/mpeg',
+			'xi-api-key': apiKey
+		},
+		body: JSON.stringify({
+			text: text,
+			model_id: 'eleven_multilingual_v2',
+			voice_settings: {
+				stability: 0.75,
+				similarity_boost: 0.75
+			}
+		})
+	});
+
+	const audio = await response.arrayBuffer();
+
+	return audio;
+}
--- src/elevenlabs/index.ts
+++ src/elevenlabs/index.ts
@@ -1,9 +1,11 @@
 import ListVoices from './funcs/ListVoices';
+import TextToSpeech from './funcs/TextToSpeech';
 
 const elevenlabs = {
 	voices: {
 		list: ListVoices
-	}
+	},
+	speak: TextToSpeech
 };
 
 export default elevenlabs;
