7a2c3c0504724b951a910799c7600302fa79cd91
merkie <merkie@post.com>
2024-08-23 17:13:58 -0500

    increase json body limit to 50mb

--- src/server/start-server.ts
+++ src/server/start-server.ts
@@ -15,7 +15,7 @@ export default async function StartServer() {
 	app.use(cors());
 	app.options('*', cors());
 
-	app.use(express.json());
+	app.use(express.json({ limit: '50mb' }));
 
 	app.use(LogRequestMiddleware);
 
