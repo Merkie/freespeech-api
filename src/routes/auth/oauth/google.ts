c0b3d223e7509f3a7eb05516154c5ab2c75aa51a
merkie <merkie@post.com>
2024-09-08 13:43:42 -0500

    get origin on process google route

--- src/routes/auth/oauth/google.ts
+++ src/routes/auth/oauth/google.ts
@@ -1,14 +1,21 @@
 import type { Request, Response } from 'express';
 import prisma from '@/resources/prisma';
 import { generateToken } from '@/utils/token';
-import { CLIENT_HOST, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '@/utils/env';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '@/utils/env';
+import { z } from 'zod';
+import { validateSchema } from '@/middleware/validate-schema';
 
-export const GET = [
+const schema = z.object({
+	origin: z.string(),
+	code: z.string()
+});
+
+export const POST = [
+	validateSchema(schema),
 	async (req: Request, res: Response) => {
-		const code = req.query.code as string;
-		if (!code) return res.status(400).json({ error: 'No code provided' });
+		const body = req.body as z.infer<typeof schema>;
 
-		const { access_token } = await getAccessToken(code);
+		const { access_token } = await getAccessToken(body.origin, body.code);
 		if (!access_token) return res.status(400).json({ error: 'No access token.' });
 
 		const profile = await getProfile(access_token);
@@ -38,14 +45,14 @@ export const GET = [
 	}
 ];
 
-async function getAccessToken(code: string) {
+async function getAccessToken(origin: string, code: string) {
 	const url = new URL('https://oauth2.googleapis.com/token');
 
 	url.searchParams.append('client_id', GOOGLE_CLIENT_ID);
 	url.searchParams.append('client_secret', GOOGLE_CLIENT_SECRET);
 	url.searchParams.append('code', code);
 	url.searchParams.append('grant_type', 'authorization_code');
-	url.searchParams.append('redirect_uri', CLIENT_HOST + '/oauth/google');
+	url.searchParams.append('redirect_uri', `${origin}/oauth/google`);
 
 	const response = await fetch(url, {
 		method: 'POST'
