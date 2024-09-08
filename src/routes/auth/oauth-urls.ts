0f573d06fe16422203d95678997283b83e4a1982
merkie <merkie@post.com>
2024-09-08 13:30:05 -0500

    add origin to oauth urls

--- src/routes/auth/oauth-urls.ts
+++ src/routes/auth/oauth-urls.ts
@@ -1,19 +1,28 @@
-import { CLIENT_HOST, GOOGLE_CLIENT_ID } from '@/utils/env';
+import { validateSchema } from '@/middleware/validate-schema';
+import { GOOGLE_CLIENT_ID } from '@/utils/env';
 import type { Request, Response } from 'express';
+import { z } from 'zod';
 
-export const GET = [
+const schema = z.object({
+	origin: z.string()
+});
+
+export const POST = [
+	validateSchema(schema),
 	async (req: Request, res: Response) => {
-		const googleUrl = getGoogleOauthUrl();
+		const body = req.body as z.infer<typeof schema>;
+
+		const googleUrl = getGoogleOauthUrl(body.origin);
 
 		return res.json({ google: googleUrl });
 	}
 ];
 
-function getGoogleOauthUrl() {
+function getGoogleOauthUrl(origin: string) {
 	const url = new URL('https://accounts.google.com/o/oauth2/v2/auth');
 
 	url.searchParams.append('client_id', GOOGLE_CLIENT_ID);
-	url.searchParams.append('redirect_uri', CLIENT_HOST + '/oauth/google');
+	url.searchParams.append('redirect_uri', `${origin}/oauth/google`);
 	url.searchParams.append('access_type', 'offline');
 	url.searchParams.append('response_type', 'code');
 	url.searchParams.append('prompt', 'consent');
