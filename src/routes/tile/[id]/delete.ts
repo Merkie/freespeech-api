2890895b3b3bf741b990bd6d31131acf07c22c78
merkie <merkie@post.com>
2024-11-10 23:08:44 -0600

    invalidate page cache when editing tiles

--- src/routes/tile/[id]/delete.ts
+++ src/routes/tile/[id]/delete.ts
@@ -1,18 +1,29 @@
 import { authenticateRequest } from '@/middleware/authenticate-request';
+import { invalidateCache } from '@/resources/cache';
 import prisma from '@/resources/prisma';
 import type { Request, Response } from 'express';
 
 export const DELETE = [
 	authenticateRequest(),
 	async (req: Request, res: Response) => {
-		await prisma.tile.delete({
-			where: {
-				id: req.params.id,
-				TilePage: {
+		const [tilePage, _] = await Promise.all([
+			prisma.tilePage.findFirst({
+				where: {
+					id: req.params.id,
 					userId: req.userId
 				}
-			}
-		});
+			}),
+			prisma.tile.delete({
+				where: {
+					id: req.params.id,
+					TilePage: {
+						userId: req.userId
+					}
+				}
+			})
+		]);
+
+		invalidateCache(`page:${tilePage}:${req.userId}`);
 
 		return res.json({ success: true });
 	}
